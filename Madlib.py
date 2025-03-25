b93b4bd65fb313bd15cf1977d497e86f5e67a2ec
ShemNderitu <shemnderituh@gmail.com>
2025-03-25 13:42:09 +0300

    Mad Lib Game

---
+++ Madlib.py
@@ -0,0 +1,11 @@
+noun1 = input("Insert 1st noun: ")
+adjective1 = input("Insert 1st adjective: ")
+noun2 = input("Insert 2nd noun: ")
+noun3 = input("Insert 3rd noun: ")
+adjective2 = input("Insert 2nd adjective: ")
+adjective3 = input("Insert 3rd adjective: ")
+
+print(f"I went to {noun1} national park")
+print(f"I saw a {adjective1} {noun2}")
+print(f"The {noun2} was eating a {noun3}")
+print(f"I was vey {adjective2} to have such a {adjective3} experience")
